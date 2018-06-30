3e550165a5a6478cf59a1d26cccab4f02022cfdf
ppopeskul <pasha.9191@mail.ru>
2018-06-30 20:26:33 +0300

    Start project

---
+++ src/scripts/z_main.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function() {
+    $('select').selectpicker();
+});
+
+$(document).ready(function() {
+    $('.slider-review').slick({});
+});
+
+$(document).ready(function() {
+    $('.promo-product').slick({
+        dots: true
+    });
+});
