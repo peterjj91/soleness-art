8ce8370e8f857b8001287ffdab364334a6a96124
ppopeskul <pasha.9191@mail.ru>
2018-07-03 20:34:21 +0300

    Index mob desktop

--- build/js/main.js
+++ build/js/main.js
@@ -31798,7 +31798,90 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-    $('.promo-product').slick({
-        dots: true
+    $('.promo-slider').slick({
+        dots: true,
+        arrows: false
+    });
+});
+
+$(document).ready(function() {
+    $('.slider-item').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1050,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    centerMode: true,
+                    // centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
+
+$(document).ready(function(){
+    // $('.promo-search__icon').click(function(){
+    //     $("#recover-step1").addClass('hidden');
+    //     $("#recover-step2").addClass('visible');
+    // });
+});
+
+function buttonUp(){
+    var valux = $('.sb-search-input').val();
+    valux = $.trim(valux).length;
+    if(valux !== 0){
+        $('.sb-search-submit').css('z-index','99');
+    } else{
+        $('.sb-search-input').val('');
+        $('.sb-search-submit').css('z-index','-999');
+    }
+}
+
+$(document).ready(function(){
+    var submitIcon = $('.sb-icon-search');
+    var submitInput = $('.sb-search-input');
+    var searchBox = $('.sb-search');
+    var isOpen = false;
+
+    $(document).mouseup(function(){
+        if(isOpen == true){
+            submitInput.val('');
+            $('.sb-search-submit').css('z-index','-999');
+            submitIcon.click();
+        }
+    });
+
+    submitIcon.mouseup(function(){
+        return false;
+    });
+
+    searchBox.mouseup(function(){
+        return false;
+    });
+
+    submitIcon.click(function(){
+        if(isOpen == false){
+            searchBox.addClass('sb-search-open');
+            $('.nav--header').addClass('nav--none');
+            isOpen = true;
+        } else {
+            searchBox.removeClass('sb-search-open');
+            $('.nav--header').removeClass('nav--none');
+            isOpen = false;
+        }
+    });
+
+    submitInput.click(function(){
+        searchBox.addClass('sb-search-open');
+        $('.nav--header').addClass('nav--none');
+        isOpen = true;
     });
 });
